fcc52242f18456e98fd2755aebf9a634b8ca58c9
sttinh99 <17110381@student.hcmute.edu.vn>
2020-09-14 08:57:23 +0700

    Updates

---
+++ precache-manifest.758c1a97f7c8e72500a2c5e9d4b3a5d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032ba6412bfb36ba218cb1eb01035729",
+    "url": "/countdown-2021/index.html"
+  },
+  {
+    "revision": "7e05e366e7b32884c2f6",
+    "url": "/countdown-2021/static/css/main.b3cfd722.chunk.css"
+  },
+  {
+    "revision": "6cfcf65d876d33e7ca25",
+    "url": "/countdown-2021/static/js/2.e4f7057c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdown-2021/static/js/2.e4f7057c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e05e366e7b32884c2f6",
+    "url": "/countdown-2021/static/js/main.fd35bb54.chunk.js"
+  },
+  {
+    "revision": "063d62c359a82830471b",
+    "url": "/countdown-2021/static/js/runtime-main.16e70fee.js"
+  },
+  {
+    "revision": "2e0413fee4a90c9498a33e033dd9836d",
+    "url": "/countdown-2021/static/media/clock.2e0413fe.mp3"
+  },
+  {
+    "revision": "d99605986ae81530a69d8444df2e31ad",
+    "url": "/countdown-2021/static/media/festival.d9960598.svg"
+  },
+  {
+    "revision": "8f949f5c7fab000e05699c0dc63a9025",
+    "url": "/countdown-2021/static/media/fireworks.8f949f5c.svg"
+  },
+  {
+    "revision": "70e46e2f652618158ccb20f41854ead2",
+    "url": "/countdown-2021/static/media/new-year-2021.70e46e2f.jpg"
+  }
+]);
