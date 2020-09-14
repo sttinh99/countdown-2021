aa7af1e6727035161ac560a262f83eb296f2aa8a
sttinh99 <17110381@student.hcmute.edu.vn>
2020-09-14 08:33:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import festival from "./images/festival.svg";
+import fireworks from "./images/fireworks.svg";
+import soundsClock from "./sounds/clock.mp3";
+
+import TimeProvider from "./components/TimeProvider";
+import TimeContext from "./contextAPI/TimeContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TimeProvider>
+      <div className="App">
+        <h1>
+          New Year 2021
+          <img src={fireworks} alt="fireworks" width="30px" height="30px" />
+          <img src={festival} alt="festival" width="30px" height="30px" />
+        </h1>
+
+        <TimeContext.Consumer>
+          {(times) => (
+            <div className="format-countdown">
+              <audio autoPlay loop>
+                <source src={soundsClock}></source>
+              </audio>
+              <h1>
+                {times.days}
+                <span> days</span>
+              </h1>
+              <h1>
+                {times.hours}
+                <span> hours</span>
+              </h1>
+              <h1>
+                {times.minutes}
+                <span> minutes</span>
+              </h1>
+              <h1>
+                {times.seconds}
+                <span> seconds</span>
+              </h1>
+            </div>
+          )}
+        </TimeContext.Consumer>
+      </div>
+      <span>by: sttinh99</span>
+    </TimeProvider>
   );
 }
-
 export default App;
---
+++ src/components/TimeProvider.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+
+import TimeContext from "../contextAPI/TimeContext";
+const NewYears = new Date("1 Jan 2021");
+
+export default class extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      const CurrentDay = new Date();
+      this.setState({
+        days: Math.floor((NewYears - CurrentDay) / (1000 * 24 * 3600)),
+        hours: Math.floor((NewYears - CurrentDay) / (1000 * 3600)) % 24,
+        minutes: (Math.floor((NewYears - CurrentDay) / (1000 * 60)) % 24) % 60,
+        seconds: Math.floor((NewYears - CurrentDay) / 1000) % 60,
+      });
+    }, 1000);
+  }
+  render() {
+    return (
+      <TimeContext.Provider
+        value={{
+          days: this.state.days,
+          hours: this.state.hours,
+          minutes: this.state.minutes,
+          seconds: this.state.seconds,
+        }}
+      >
+        {this.props.children}
+      </TimeContext.Provider>
+    );
+  }
+}
